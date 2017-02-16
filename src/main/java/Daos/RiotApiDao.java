bcfa51ff33d72b3d59a18d2c7aa508dba27114a9
d00165195 <d00165195@P1111-PC39.ad.dkit.ie>
2017-02-16 09:57:11 +0000

    API

---
+++ src/main/java/Daos/RiotApiDao.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Daos;
+
+/**
+ *
+ * @author Chris
+ */
+import java.util.Map;
+import net.rithms.riot.constant.Region;
+import net.rithms.riot.dto.Summoner.Summoner;
+import net.rithms.riot.api.RiotApi;
+import net.rithms.riot.api.RiotApiException;
+import com.google.gson*;
+
+public class RiotApiDao {
+    
+}
