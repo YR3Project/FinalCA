278778d465c1c0a97c15338e7c2dad2729cde53b
AleksMtt <AleksMtt@10.102.11.83>
2017-03-02 11:17:24 +0000

    DB

--- src/main/java/SQL/swgw.sql
+++ src/main/java/SQL/swgw.sql
@@ -7,10 +7,10 @@ CREATE TABLE article (
   Title varchar(50) NOT NULL,
   ArticleText varchar(2000) NOT NULL,
   DateAdded date NOT NULL
-  FOREIGN KEY (`AuthorID`) REFERENCES `users` (`userID`); 
+  FOREIGN KEY (AuthorID) REFERENCES users (userID)); 
 
 
-INSERT INTO `article` (`ArticleID`, `AuthorID`, `Title`, `ArticleText`, `DateAdded`) VALUES
+INSERT INTO article (ArticleID, AuthorID, Title, ArticleText, DateAdded) VALUES
 (1, 1, 'StatWiseGameWise is born!', 'Yes Indeed! The website is finally born! You all better enjoy your time here', '2017-02-06'),
 (2, 3, 'New Article', 'word word word word word word word word word word word word word word word word word word word word word word word word', '2017-02-07');
 
