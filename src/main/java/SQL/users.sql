d3b8866685e34b59d9d7ac4cf41acc1e50249227
ben <ben@ben-PC1>
2017-05-01 19:39:30 +0100

    new user table

--- src/main/java/SQL/users.sql
+++ src/main/java/SQL/users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2017 at 04:50 PM
+-- Generation Time: May 01, 2017 at 08:38 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -31,6 +31,7 @@ CREATE TABLE `users` (
   `username` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
   `password` varchar(128) NOT NULL,
+  `country` varchar(200) DEFAULT 'No Country',
   `admin` smallint(1) NOT NULL,
   `salt` varchar(128) NOT NULL,
   `Created` varchar(20) NOT NULL,
@@ -42,11 +43,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `username`, `email`, `password`, `admin`, `salt`, `Created`, `Due`, `photo`) VALUES
-(46, 'Chris', 'chris.mck@gmail.com', 'c1cdb3ada9de6cfe84c3a7cfe192bcbda2331130c8db4d5026a1ae250be4af60974fcddf1903afcb03f97c9976e0e6e0aa8d991d44d6a43fe3a6af92399a0806', 1, '„›ærº\n²…Bü=˜Q', '08/04/17', '18/04/17', 'Images/Profiles/46.jpg'),
-(47, 'AleksMtr', 'aleks56@gmail.com', 'bb6ca5a73b4b82ad50789d156a770cf8ed3279df13caceb9ea465b94152c165361c32460790b944b2bd6f8ca5404fda9dc821d9044cc9bc203f8c1e63f717a73', 1, 'ìÄNÐôG–uÛ5)ê', '08/04/17', '18/04/17', 'Images/Profiles/47.jpg'),
-(48, 'BenRose', 'ben.rose@gmail.com', 'b025b32a87802b94541d75aca4e27248de2cf1ec5c6cd5cf05fabfab13b7cc48a002edf4a4363ab15dccd38b72853fcefdc74c932c4d2e3054665af0466de5fc', 1, 'Lð[\ZÜX¿ˆb¨ˆ‰…', '16/04/17', '05/12/17', 'Images/Profiles/35.jpg'),
-(51, 'JohnWick67', 'ben.rose76@gmail.com', '0068212c622b9b18c9bb4f95fce9312a438ab5ba0438154e4210bc3ef7da9c891145cd841757f1965e92f2334ae35e6b795f3895389de9d201cb16d0c2ee20ae', 0, 'Ñù€}4‡ÖÀQ í ·GAï', '17/04/17', '18/04/17', 'Images/Profiles/Default.jpg');
+INSERT INTO `users` (`userID`, `username`, `email`, `password`, `country`, `admin`, `salt`, `Created`, `Due`, `photo`) VALUES
+(46, 'Chris', 'chris.mck@gmail.com', 'c1cdb3ada9de6cfe84c3a7cfe192bcbda2331130c8db4d5026a1ae250be4af60974fcddf1903afcb03f97c9976e0e6e0aa8d991d44d6a43fe3a6af92399a0806', 'Ireland', 0, '„›ærº\n²…Bü=˜Q', '08/04/17', '18/04/17', 'Images/Profiles/46.jpg'),
+(47, 'AleksMtr', 'aleks56@gmail.com', 'bb6ca5a73b4b82ad50789d156a770cf8ed3279df13caceb9ea465b94152c165361c32460790b944b2bd6f8ca5404fda9dc821d9044cc9bc203f8c1e63f717a73', 'Ireland', 1, 'ìÄNÐôG–uÛ5)ê', '08/04/17', '18/04/17', 'Images/Profiles/47.jpg'),
+(48, 'BenRose', 'ben.rose@gmail.com', '44c762f1b0c34b1b1a85ee47ed75c838f86eb9c88c775cb5682756850608e6050cb9680254a355219a3e0aa83e1e4669bb62c653d3f19b7fc175be81fcd71e63', 'Ireland', 1, 'mÚ—&\0BË\\ç×¿¼É\Z', '21/04/17', '20/05/17', 'Images/Profiles/35.jpg');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +66,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `userID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
